89cb35f27ede14b3f6e0c39e02d0fc1dc6ca8e72
Eduardo Duarry <eduarry@hotmail.com>
2023-03-28 11:43:37 +0200

    Primer deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'clinica-safa',
+    appId: '1:774492577242:web:1cb7fc9ec9381e5649f4e3',
+    storageBucket: 'clinica-safa.appspot.com',
+    apiKey: 'AIzaSyCU8EOwaYD0D2GLleBrXVCPx3SGBXmVJn0',
+    authDomain: 'clinica-safa.firebaseapp.com',
+    messagingSenderId: '774492577242',
+    measurementId: 'G-EFMLFXEJLY',
+  },
   production: false
 };
 
