ef61b003bcb89ebf63ddb9d0338dec0be46efe90
Eduardo Duarry <eduarry@hotmail.com>
2023-03-28 10:45:24 +0200

    Registro hecho

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Form, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  form = this.formBuilder.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.minLength(6), Validators.required]],
+    confirmPassword: ['', [Validators.minLength(6), Validators.required]],
+  });
+
+  showError = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  register() {
+    if (this.form.valid) {
+      const { email, password, confirmPassword } = this.form.getRawValue();
+      console.log(email, password);
+      this.showError = false;
+    } else {
+      this.form.markAsTouched();
+      this.showError = true;
+    }
   }
 
 }
