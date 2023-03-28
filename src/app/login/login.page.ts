88201b8b230b7ecd1c169f9f65efa94b4cbd7a66
Eduardo Duarry <eduarry@hotmail.com>
2023-03-28 10:07:23 +0200

    Login
    Login terminado

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Form, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  form = this.formBuilder.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.minLength(6), Validators.required]],
+  });
+
+  showError = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    if (this.form.valid) {
+      const { email, password } = this.form.getRawValue();
+      console.log(email, password);
+      this.showError = false;
+    } else {
+      this.form.markAsTouched();
+      this.showError = true;
+    }
   }
 
 }
